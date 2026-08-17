7a3e7107674a47cb6f740b0d4cb2bb663ae46962
loriscesati <loriscesati@gmail.com>
2026-08-17 23:11:12 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+// Service Worker per Low FODMAP Tracker
+// Incrementa CACHE_NAME ad ogni modifica ai file per forzare l'aggiornamento
+const CACHE_NAME = 'fodmap-v1';
+
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.svg',
+  './icon-512.svg'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Solo richieste GET dello stesso dominio
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        // Aggiorna la cache con l'ultima versione ricevuta dalla rete
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
